bb5271bb30897b54b73e11bdc31b055d1bbe6a6e
lavagancia <potola30@gmail.com>
2024-03-05 14:35:21 -0400

    update

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "bandeau",
+      artist: "la banda del aguante",
+      img: "2005",
+      src: "banda"
+    },
+    {
+      name: "No me arrepiento de este amor",
+      artist: "La Vagancia - Potola",
+      img: "nomearrepiento",
+      src: "nomearrepiento"
+    },
+    {
+      name: "Desde la cuna",
+      artist: "POTOLA-VAGANCIA",
+      img: "Desdeelacuna",
+      src: "desdeelacuna"
+    },
+    {
+      name: "ORIENTISIMO",
+      artist: "Los pascaneros",
+      img: "Orientisimo",
+      src: "Orientisimo"
+    },
+    {
+      name: "Oriente Petrolero 1971",
+      artist: "Los pascaneros",
+      img: "taquirari1971",
+      src: "taquirari1971"
+    },
+    {
+      name: "mix los de siempre",
+      artist: "POTOLA-VAGANCIA",
+      img: "music-6",
+      src: "esunalokura"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
